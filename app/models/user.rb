e1312b6a280e75e16b0d8348badafc302982ce6a
aminyasser <aminyasser88@gmail.com>
2022-02-21 15:52:56 +0200

    dahsboard changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable 
+  validates :name, presence: true , length: { minimum: 3, maximum: 50 }
   has_many :seats , dependent: :destroy
 end
